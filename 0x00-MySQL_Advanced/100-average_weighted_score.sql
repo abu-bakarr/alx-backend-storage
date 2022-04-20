1b2134b8745642a949a8a1377d2ca56f41b4978f
abu-bakarr <abangura14@yahoo.com>
2022-04-20 09:47:23 +0000

    Add exer 12

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,8 @@
+-- Average weighted score
+DELIMITER |
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(IN user_id INT)
+BEGIN 
+DECLARE average FLOAT;
+SET average = (SELECT (SUM(score * weight)/SUM(weight)) FROM corrections AS newTable INNER JOIN projects ON project_id = id WHERE newTable.user_id = user_id);
+UPDATE users SET average_score=average WHERE id=user_id;
+END;
