58d0bced7a19719046a2255f12794dd26841676a
abu-bakarr <abangura14@yahoo.com>
2022-04-20 09:38:24 +0000

    Add exer 6

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -6,4 +6,4 @@ BEGIN
     UPDATE items
     SET quantity = quantity - NEW.number
     WHERE name = NEW.item_name;
-END$$
+END$$;
---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,9 @@
+-- valid email
+DELIMITER |
+CREATE TRIGGER bool BEFORE UPDATE ON users
+FOR EACH ROW 
+BEGIN
+    IF NEW.email <> OLD.email THEN 
+    SET NEW.valid_email = 0;
+    END IF;
+END;
